220ace180d7c1de0a0499fda6a6419655861dd8a
detafebbyariesta <detafebbyariesta@gmail.com>
2021-03-30 17:24:45 +0700

    pertama

---
+++ lib/dbhelper/dbhelper.dart
@@ -0,0 +1,97 @@
+import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import '../model/item.dart';
+
+class DbHelper {
+  static DbHelper _dbHelper;
+  static Database _database;
+  DbHelper._createObject();
+  Future<Database> initDb() async {
+//untuk menentukan nama database dan lokasi yg dibuat
+    Directory directory = await getApplicationDocumentsDirectory();
+    String path = directory.path + 'item.db';
+//create, read databases
+    var itemDatabase = openDatabase(path,
+        version: 8,
+        onCreate: _createDb,
+        onUpgrade:
+            _onUpgrade); //mengembalikan nilai object sebagai hasil dari fungsinya
+    return itemDatabase;
+  }
+
+  void _onUpgrade(Database db, int oldVersion, int newVersion) async {
+    _createDb(db, newVersion);
+  }
+
+//buat tabel baru dengan nama item
+  void _createDb(Database db, int version) async {
+    var batch = db.batch();
+    await batch.execute('DROP TABLE IF EXISTS item');
+    await batch.execute('''
+    
+    CREATE TABLE item (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nama TEXT,
+    namaBarang TEXT,
+    price INTEGER,
+    stok INTEGER,
+    kodeBarang INTEGER
+    )
+    ''');
+    await batch.commit();
+  }
+
+//select databases
+  Future<List<Map<String, dynamic>>> select() async {
+    Database db = await this.initDb();
+    var mapList = await db.query('item', orderBy: 'namaBarang');
+    return mapList;
+  }
+
+//create databases
+  Future<int> insert(Item object) async {
+    Database db = await this.initDb();
+    int count = await db.insert('item', object.toMap());
+    return count;
+  }
+
+//update databases
+  Future<int> update(Item object) async {
+    Database db = await this.initDb();
+    int count = await db
+        .update('item', object.toMap(), where: 'id=?', whereArgs: [object.id]);
+    return count;
+  }
+
+  //delete databases
+  Future<int> delete(int id) async {
+    Database db = await this.initDb();
+    int count = await db.delete('item', where: 'id=?', whereArgs: [id]);
+    return count;
+  }
+
+  Future<List<Item>> getItemList() async {
+    var itemMapList = await select();
+    int count = itemMapList.length;
+    List<Item> itemList = List<Item>();
+    for (int i = 0; i < count; i++) {
+      itemList.add(Item.fromMap(itemMapList[i]));
+    }
+    return itemList;
+  }
+
+  factory DbHelper() {
+    if (_dbHelper == null) {
+      _dbHelper = DbHelper._createObject();
+    }
+    return _dbHelper;
+  }
+  Future<Database> get database async {
+    if (_database == null) {
+      _database = await initDb();
+    }
+    return _database;
+  }
+}
---
+++ lib/model/entryform.dart
@@ -0,0 +1,208 @@
+import 'package:flutter/material.dart';
+import '../model/item.dart';
+
+class EntryForm extends StatefulWidget {
+  final Item item;
+  EntryForm(this.item);
+  @override
+  EntryFormState createState() => EntryFormState(this.item);
+}
+
+//class controller
+class EntryFormState extends State<EntryForm> {
+  Item item;
+  EntryFormState(this.item);
+  TextEditingController namaController = TextEditingController();
+  TextEditingController namaBarangController = TextEditingController();
+  TextEditingController priceController = TextEditingController();
+  TextEditingController stokController = TextEditingController();
+  TextEditingController kodeBarangController = TextEditingController();
+  TextEditingController tanggalController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+//kondisi
+    if (item != null) {
+      namaController.text = item.nama;
+      namaBarangController.text = item.namaBarang;
+      priceController.text = item.price.toString();
+      stokController.text = item.stok;
+      kodeBarangController.text = item.kodeBarang.toString();
+      // tanggalController.text = item.tanggal;
+    }
+//rubah
+    return Scaffold(
+      appBar: AppBar(
+        title: item == null ? Text('Tambah') : Text('Ubah'),
+        leading: Icon(Icons.keyboard_arrow_left),
+      ),
+      body: Padding(
+        padding: EdgeInsets.only(top: 15.0, left: 10.0, right: 10.0),
+        child: ListView(
+          children: <Widget>[
+// nama
+            Padding(
+              padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+              child: TextField(
+                controller: namaController,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  labelText: 'Nama Pembeli',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                onChanged: (value) {
+//
+                },
+              ),
+            ),
+// nama barang
+            Padding(
+              padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+              child: TextField(
+                controller: namaBarangController,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  labelText: 'Nama Barang',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                onChanged: (value) {
+//
+                },
+              ),
+            ),
+// harga
+            Padding(
+              padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+              child: TextField(
+                controller: priceController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Harga',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                onChanged: (value) {
+                  //
+                },
+              ),
+            ),
+// stok
+            Padding(
+              padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+              child: TextField(
+                controller: stokController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Stok',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                onChanged: (value) {
+                  //
+                },
+              ),
+            ),
+// kode barang
+            Padding(
+              padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+              child: TextField(
+                controller: kodeBarangController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Kode Barang',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                
+                ),
+                onChanged: (value) {
+//
+                },
+              ),
+            ),
+// tanggal
+//             Padding(
+//               padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+//               child: TextField(
+//                 controller: tanggalController,
+//                 keyboardType: TextInputType.datetime,
+//                 decoration: InputDecoration(
+//                   labelText: 'Tanggal',
+//                   border: OutlineInputBorder(
+//                     borderRadius: BorderRadius.circular(5.0),
+//                   ),
+//                 ),
+//                 onChanged: (value) {
+// //
+//                 },
+//               ),
+//             ),
+// tombol button
+            Padding(
+              padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+              child: Row(
+                children: <Widget>[
+// tombol simpan
+                  Expanded(
+                    child: RaisedButton(
+                      color: Theme.of(context).primaryColorDark,
+                      textColor: Theme.of(context).primaryColorLight,
+                      child: Text(
+                        'Save',
+                        textScaleFactor: 1.5,
+                      ),
+                      onPressed: () {
+                        if (item == null) {
+// tambah data
+                          item = Item(
+                              namaController.text,
+                              namaBarangController.text,
+                              int.parse(priceController.text),
+                              int.parse(stokController.text),
+                              int.parse(kodeBarangController.text));
+                              // tanggalController.text);
+                        } else {
+// ubah data
+                          item.nama = namaController.text;
+                          item.namaBarang = namaBarangController.text;
+                          item.price = int.parse(priceController.text);
+                          item.stok = int.parse(stokController.text);
+                          item.kodeBarang = int.parse(kodeBarangController.text);
+                          // item.tanggal = tanggalController.text;
+                        }
+// kembali ke layar sebelumnya dengan membawa objek item
+                        Navigator.pop(context, item);
+                      },
+                    ),
+                  ),
+                  Container(
+                    width: 5.0,
+                  ),
+// tombol batal
+                  Expanded(
+                    child: RaisedButton(
+                      color: Theme.of(context).primaryColorDark,
+                      textColor: Theme.of(context).primaryColorLight,
+                      child: Text(
+                        'Cancel',
+                        textScaleFactor: 1.5,
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
